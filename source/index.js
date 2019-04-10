7fee9be89a2310fdf26f5015659e0a12ced6cbd0
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2019-04-10 18:58:28 +0200

    Consistency.

--- source/index.js
+++ source/index.js
@@ -26,7 +26,7 @@ export default function mapSort(list, mapCallback, compareFunction) {
 		compareFunction = defaultCompareFunction;
 	}
 	// Sort the indexes by looking up and comparing the "sortable" values associated with those indexes.
-	indexes.sort((aIndex, bIndex) => compareFunction(sortables[aIndex], sortables[bIndex]));
+	indexes.sort((firstIndex, secondIndex) => compareFunction(sortables[firstIndex], sortables[secondIndex]));
 	// The indexes array is now in the correct order. Create a new array which contains the original values, but in that
 	// correct order. This array is the result.
 	return indexes.map(index => list[index]);
