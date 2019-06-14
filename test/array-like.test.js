57f54726b534844acbd912455cec1e30518ae032
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2019-06-14 16:29:34 +0200

    Added a test to ensure the implementation handles array-like input well.

---
+++ test/array-like.test.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment node
+ */
+
+import mapSort from '..';
+
+function identity(input) {
+	return input;
+}
+test('array-like', () => {
+	// This tests whether the implementation handles array-like objects well.
+	const list = {
+		length: 0
+	};
+	[].push.call(list, 'd', 'c', 'f');
+	const actualResult = mapSort(
+		list,
+		identity
+	);
+	expect(actualResult)
+	.toEqual(['c', 'd', 'f']);
+});
