aa655229d33c7c799135c26e6e1dbdaa3e4079a5
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2019-08-03 10:40:07 +0200

    Simplified the rollup configuration.

--- rollup.config.js
+++ rollup.config.js
@@ -4,35 +4,25 @@ const minify = require('rollup-plugin-babel-minify');
 
 const packageConfiguration = require('./package.json');
 
-const input = 'source/index.js';
-const plugins = [
-	babel(),
-	minify({ comments: false })
-];
-export default [
-	{
-		input,
-		output: {
+export default {
+	input: path.join('source', 'index.js'),
+	output: [
+		{
 			format: 'esm',
 			file: path.join('compiled', 'esm', `${packageConfiguration.name}.min.js`)
 		},
-		plugins
-	},
-	{
-		input,
-		output: {
+		{
 			format: 'cjs',
 			file: path.join('compiled', 'cjs', `${packageConfiguration.name}.min.js`)
 		},
-		plugins
-	},
-	{
-		input,
-		output: {
+		{
 			format: 'iife',
 			file: path.join('compiled', 'iife', `${packageConfiguration.name}.min.js`),
 			name: 'mapSort'
-		},
-		plugins
-	}
-];
+		}
+	],
+	plugins: [
+		babel(),
+		minify({ comments: false })
+	]
+};
