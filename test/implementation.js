a7cfad43021db381f9d8c3c1c7f6fa51e6900bee
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2019-10-01 12:40:20 +0200

    Micro cleanup.

--- test/implementation.js
+++ test/implementation.js
@@ -1,6 +1,6 @@
 var mapSort;
 // For regular tests, using the compiled version makes the most sense as it is the most realistic version…
-/* istanbul ignore next */
+/* istanbul ignore if */
 if ('coverage' != process.env.TEST_TYPE) {
 	mapSort = require('..');
 // …however, when testing coverage, the compiled version gives skewed results.
