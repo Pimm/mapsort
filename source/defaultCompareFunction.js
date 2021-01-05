41609b42ad49c077e16246685eab3d7f5cd06416
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2021-01-05 14:03:11 +0100

    Likely improved performance of the defaultCompareFunction.
    This adds one character to the compiled files, but it should improve the performance when the default compare function is used. Before, it checked a < b, and if false, it would check a > b. Those are two inequality checks. Now it checks a < b, and if false, it checks a == b. One inequality check and one equality check.

--- source/defaultCompareFunction.js
+++ source/defaultCompareFunction.js
@@ -1,6 +1,6 @@
 /**
  * This is the function which can be passed to Array.prototype.sort to make it behave as if no function had been
- * passed.
+ * passed. It returns the lexicographic order of the passed two elements.
  *
  * This function mimics 5 through 11 of SortCompare as defined in ECMAScript 2015.
  */
@@ -9,9 +9,9 @@ export default function defaultCompareFunction(first, second) {
 	const secondAsString = String(second);
 	if (firstAsString < secondAsString) {
 		return -1;
-	} else if (firstAsString > secondAsString) {
-		return 1;
-	} else /* if (firstAsString == secondAsString) */ {
+	} else if (firstAsString == secondAsString) {
 		return 0;
+	} else /* if (firstAsString > secondAsString) */ {
+		return 1;
 	}
 }
